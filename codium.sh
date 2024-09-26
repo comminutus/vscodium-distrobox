150e2ef6a1c225180b0fa19ee14b8afdf2363cf2
Comminutus <satsang_kammen@jnk.bz>
2024-09-26 07:57:57 -0400

    Update codium.sh script

--- codium.sh
+++ codium.sh
@@ -4,7 +4,7 @@
 set -o pipefail
 
 if [ -z "${CONTAINER_ID}" ]; then
-    /usr/bin/distrobox-enter -n vscodium-distrobox -- '/usr/bin/codium' --no-url-handler --enable-features=WaylandWindowDecorations --ozone-platform=wayland --class=VSCodium "$@" 2> /dev/null | grep -v '^Warning:'
+    /usr/bin/distrobox-enter -n vscodium -- '/usr/bin/codium' --no-url-handler --enable-features=WaylandWindowDecorations --ozone-platform=wayland --class=VSCodium "$@" 2> /dev/null | grep -v '^Warning:'
     EXIT_STATUS=${PIPESTATUS[0]}
 elif [ -n "${CONTAINER_ID}" ] && [ "${CONTAINER_ID}" != "vscodium-distrobox" ]; then
     distrobox-host-exec "$HOME/.local/bin/codium" --no-url-handler --enable-features=WaylandWindowDecorations --ozone-platform=wayland --class=VSCodium "$@" 2> /dev/null | grep -v '^Warning:'
